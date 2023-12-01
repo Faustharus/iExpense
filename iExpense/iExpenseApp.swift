26e77a629a9fafe3cd2868bb4934407506b1bd23
Faustharus <chailloleau-damien@outlook.fr>
2023-12-01 16:45:52 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Damien Chailloleau on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
